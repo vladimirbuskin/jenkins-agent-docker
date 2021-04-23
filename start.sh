6558fe723ff7518ec7c34952e78ba06bf5594dd6
Vladimir <vladimir.buskin@gmail.com>
2021-04-22 23:07:58 -0700

    .

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --name jba vladimirbuskin/jba \
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --name jad vladimirbuskin/jad \
   java -jar agent.jar -jnlpUrl http://<jenkins-host>/computer/<jenkins-agent-name>/jenkins-agent.jnlp -secret <jenkins-secret> -workDir "/var/jenkins"
